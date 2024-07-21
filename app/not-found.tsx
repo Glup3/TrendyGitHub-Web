d44265bdcb28662e853cb47ad047545013d85edb
Phuc Tran <glup3.tran@gmail.com>
2024-07-21 12:29:52 +0200

    add simple 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="container">
+      <h1 className="text-4xl font-bold">404</h1>
+      <h2 className="text-xl font-semibold">Sorry we couldn&apos;t find this page.</h2>
+    </main>
+  )
+}
